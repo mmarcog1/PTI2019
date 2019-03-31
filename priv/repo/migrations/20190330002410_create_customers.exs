a726d85d42305d3321e05fde5f46154a29baf5bc
mmarcog1 <mmarcog1@gmx.com>
2019-03-31 15:54:46 +0200

    Added migration

--- priv/repo/migrations/20190330002410_create_customers.exs
+++ priv/repo/migrations/20190330002410_create_customers.exs
@@ -5,6 +5,7 @@ defmodule Prototype.Repo.Migrations.CreateCustomers do
     create table(:customers) do
       add :name, :string
       add :description, :text
+      add :report, :text
       add :user_id, references(:users, on_delete: :nothing)
 
       timestamps()
